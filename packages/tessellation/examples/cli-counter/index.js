d84118a9c580e74b13877cb7bf8fa81d09ca1288
Xavier Via <fernando.via@gmail.com>
2016-12-03 20:13:45 +0100

    Add CLI example

---
+++ packages/tessellation/examples/cli-counter/index.js
@@ -0,0 +1,33 @@
+const createApp = require('tessellation/createApp').default
+const chalk = require('chalk')
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD':
+      return Object.assign({}, state, {
+        value: state.value + 1
+      })
+
+    default:
+      return reducer(state, action)
+  }
+}
+
+const initialState = {value: 0}
+
+const effects = [
+  (push) => {
+    process.stdin.on('data', () => {
+      push({
+        type: 'ADD'
+      })
+    })
+
+    return (state) => {
+      console.log('The counter value is:', chalk.red(state.value))
+      console.log(chalk.gray(`Press ${chalk.white('ENTER')} to increase it`))
+    }
+  }
+]
+
+createApp(reducer, initialState, effects)
